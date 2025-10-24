9e2e230d2a016f83da3fef091b8a6667768379da
syairaarif <putrisyafira4444@gmail.com>
2025-10-24 13:52:32 +0700

    Menambahkan Komponen Image untuk menampilkan gambar ditengah dan bulet

--- app/src/main/java/com/example/myuipertama/PraktikumQuestAdvanceLayout.kt
+++ app/src/main/java/com/example/myuipertama/PraktikumQuestAdvanceLayout.kt
@@ -1,11 +1,18 @@
 package com.example.myuipertama
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -16,7 +23,15 @@ fun Praktikum1 (modifier: Modifier) {
             .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
     ){
-
+        Image(
+            painter = painterResource(id = R.drawable.syafira),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(100.dp)
+                .background(Color.Gray, shape = CircleShape)
+                .padding(2.dp)
+        )
     }
 
 }
