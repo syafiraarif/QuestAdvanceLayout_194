c7610a41adbdfa5a78ddf1a756d9e564dc8e2539
syairaarif <putrisyafira4444@gmail.com>
2025-10-24 14:15:46 +0700

    Menambahkan Layout Row supaya bisa urut secara horizontal

--- app/src/main/java/com/example/myuipertama/PraktikumQuestAdvanceLayout.kt
+++ app/src/main/java/com/example/myuipertama/PraktikumQuestAdvanceLayout.kt
@@ -2,8 +2,12 @@ package com.example.myuipertama
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
@@ -32,6 +36,14 @@ fun Praktikum1 (modifier: Modifier) {
                 .background(Color.Gray, shape = CircleShape)
                 .padding(2.dp)
         )
+        Spacer(modifier = Modifier.height(height = 30.dp))
+
+        Row (
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ){
+
+        }
     }
 
 }
