e7fa8e48f76f343529dc0ac7818fd679d4ea76ee
syairaarif <putrisyafira4444@gmail.com>
2025-10-24 14:44:17 +0700

    Menambahkan komponen string supaya menampilkan tulisan nama

--- app/src/main/java/com/example/myuipertama/PraktikumQuestAdvanceLayout.kt
+++ app/src/main/java/com/example/myuipertama/PraktikumQuestAdvanceLayout.kt
@@ -14,13 +14,17 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun Praktikum1 (modifier: Modifier) {
@@ -107,6 +111,14 @@ fun Praktikum1 (modifier: Modifier) {
                     )
                 }
             }
+            Spacer(modifier = Modifier.height(height = 16.dp))
+
+            Text(
+                text = stringResource(id = R.string.nama),
+                fontWeight = FontWeight.Bold,
+                fontSize = 22.sp,
+                color = Color.White
+            )
         }
     }
 }
