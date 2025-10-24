b9c1e858bf285174c3a788756b74a17a8167535d
syairaarif <putrisyafira4444@gmail.com>
2025-10-24 10:08:08 +0700

    mengubah di mainactivity biar ActivitasPertama bisa di paggil

--- app/src/main/java/com/example/myuipertama/MainActivity.kt
+++ app/src/main/java/com/example/myuipertama/MainActivity.kt
@@ -20,8 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyUIPertamaTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    ActivitasPertama(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
