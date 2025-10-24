859e62753565875bfdd2e6214862439b8087f877
syairaarif <putrisyafira4444@gmail.com>
2025-10-24 09:22:51 +0700

    menambahkan komponen text pertama yang akan menampilkan prodi

--- app/src/main/java/com/example/myuipertama/UIPertama.kt
+++ app/src/main/java/com/example/myuipertama/UIPertama.kt
@@ -1,12 +1,16 @@
 package com.example.myuipertama
 
+import android.R.attr.text
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun ActivitasPertama(modifier: Modifier) {
@@ -16,6 +20,14 @@ fun ActivitasPertama(modifier: Modifier) {
             .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        text(
+            text = stringResource(id = R.string.prodi),
+            fontSize = 35.sp,
+            fontWeight = FontWeight.Bold
+        )
+
+
+
 
     }
 }
