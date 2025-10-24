4082e07a7b176e009150d7c297003c3e35877ee8
syairaarif <putrisyafira4444@gmail.com>
2025-10-24 13:46:58 +0700

    Menambahkan Layout Column agar posisi bisa vertikal

--- app/src/main/java/com/example/myuipertama/PraktikumQuestAdvanceLayout.kt
+++ app/src/main/java/com/example/myuipertama/PraktikumQuestAdvanceLayout.kt
@@ -1,9 +1,22 @@
 package com.example.myuipertama
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun Praktikum1 (modifier: Modifier) {
+    Column (
+        modifier = Modifier
+            .padding (top = 100.dp)
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+
+    }
 
 }
